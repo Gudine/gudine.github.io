35716ddf1fca9882363072fc8f0a47519599b7e7
Gudine <gudine283@gmail.com>
2022-01-03 19:22:46 -0300

    Adiciona versão em inglês

---
+++ lang.js
@@ -0,0 +1,56 @@
+const langData = {
+  pt: {
+    headers: [
+      "Guilherme&nbsp;Henrique Estevão&nbsp;Cunha",
+      "Habilidades",
+      "Python",
+      "JavaScript",
+      "HTML",
+      "CSS",
+      "Projetos",
+      "Exercícios da Trybe",
+    ],
+    toc: [
+      "Início",
+      "Habilidades",
+      "Python",
+      "JavaScript",
+      "HTML",
+      "CSS",
+      "Projetos",
+      "Exercícios da Trybe",
+    ],
+    content: [
+      "Olá, eu me chamo <strong>Guilherme</strong>! Atualmente tenho 19 anos e moro em <em>Nova Iguaçu</em>, no estado do <em>Rio de Janeiro</em>. Sempre tive uma certa paixão pela programação e, através do meu repertório sempre crescente de habilidades, sou capaz de fazer diversos tipos de trabalho.",
+      "Python foi a primeira linguagem que eu aprendi, e já tenho uma grande familiaridade com ela. Sei muitos dos métodos e funções de cór, e os que não sei, consigo achar com algumas pesquisas rápidas.",
+      "No JavaScript, em comparação ao Python, sou um pouco mais amador, mas as aulas da Trybe com certeza me farão ser tão bom nele quanto em Python, ou até melhor. A sua variedade de usos também significa que eu não poderia perder esse 'canivete' das linguagens.",
+      "HTML também é um conhecimento recente para mim, mas eu já conheço bastante dele. Sou capaz de criar páginas básicas e médias, fazendo uso de tags semânticas para facilitar a leitura por métodos alternativos.",
+      "Mesmo que recente, acredito que meu conhecimento sobre CSS seja bem alto. Estou acostumado a usar Grid e Flex, e sei de muitos modos de conseguir uma aparência desejada. No entanto, meu senso de estética não é tão bom...",
+      "Esses exercícios, feitos no decorrer do curso da Trybe, serviram para praticar as várias habilidades que foram adquiridas por mim. Inclui muitos exemplos de HTML, CSS, JavaScript, e irá incluir muito mais com o tempo.",
+    ]
+  },
+  en: {
+    headers: [
+      null,
+      "Skills",
+      null, null, null, null,
+      "Projects",
+      "Trybe Exercises",
+    ],
+    toc: [
+      "Start",
+      "Skills",
+      null, null, null, null,
+      "Projects",
+      "Trybe Exercises",
+    ],
+    content: [
+      "Hello, my name is <strong>Guilherme</strong>! Currently, I'm 19 years old and I live in <em>Nova Iguaçu</em>, in the <em>Rio de Janeiro</em> state. I've always had a passion for programming and, through my evergrowing list of capabilities, I'm able to do many kinds of work.",
+      "Python was the first language I've learned, and I'm very familiar with it. I know most of the methods and functions, and I can find the ones I don't with just a few quick searches.",
+      "In JavaScript, unlike with Python, I'm more of an amateur, but my studies at Trybe will surely make me as good at it as in Python, or even better. Its variety of uses also means I couldn't miss this multipurpose tool.",
+      "HTML is also a new development for me, but I already know a fair amount about it. I'm able to make simple and moderately complicated pages, making use of semantic tags to simplify reading through alternative methods.",
+      "Though recent, I believe my knowledge about CSS is fairly large. I'm used to use Grid and Flex, and I know many ways of getting to the appearance I desire. However, my sense of aesthetic isn't all that good...",
+      "These exercises, made during the Trybe course, were designed to practice the diverse skills I have acquired. There are many examples of HTML, CSS, JavaScript, and there'll be much more in the future.",
+    ]
+  },
+};
--- script.js
+++ script.js
@@ -1,3 +1,46 @@
-let menubars = document.querySelector('.menubars');
+let lang = 'pt';
 
-menubars.addEventListener('click', (ev) => menubars.classList.toggle('active'));
+const menubars = document.querySelector('.menubars');
+const dropdown = document.querySelector('.dropdown');
+const langBtns = [...document.querySelectorAll('.lang div')];
+
+document.body.addEventListener('click', function() {
+  menubars.classList.remove('active');
+});
+dropdown.addEventListener('click', function(ev) {
+  ev.stopPropagation();
+});
+menubars.addEventListener('click', function(ev) {
+  menubars.classList.toggle('active');
+  ev.stopPropagation();
+});
+
+langBtns.forEach((btn) => btn.addEventListener('click', changeLang))
+
+function changeLang(ev) {
+  let newLang = ev.target.innerText.toLowerCase();
+  if (newLang != lang) {
+    lang = newLang;
+    updateLang();
+  }
+}
+
+function updateLang() {
+  const headers = [...document.querySelectorAll('h1, h2, h3')];
+  const toc = [...document.querySelectorAll('.dropdown a')];
+  const content = [...document.querySelectorAll('.textblock p')];
+
+  updateGroupLang('headers', headers);
+  updateGroupLang('toc', toc);
+  updateGroupLang('content', content);
+}
+
+function updateGroupLang(group, elems) {
+  for (let i = 0; i < elems.length; i += 1) {
+    if (langData[lang][group][i] === null) {
+      elems[i].innerHTML = langData['pt'][group][i];
+    } else {
+      elems[i].innerHTML = langData[lang][group][i];
+    }
+  }
+}
