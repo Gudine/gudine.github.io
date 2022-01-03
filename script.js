ba7892f69b14dc238a554b537efb04c5de8c91a9
Gudine <gudine283@gmail.com>
2022-01-03 19:38:53 -0300

    Adiciona tradução de texto alternativo

--- script.js
+++ script.js
@@ -17,11 +17,13 @@ menubars.addEventListener('click', function(ev) {
 
 langBtns.forEach((btn) => btn.addEventListener('click', changeLang))
 
-function changeLang(ev) {
-  let newLang = ev.target.innerText.toLowerCase();
-  if (newLang != lang) {
-    lang = newLang;
-    updateLang();
+function updateGroupLang(group, elems) {
+  for (let i = 0; i < elems.length; i += 1) {
+    if (langData[lang][group][i] === null) {
+      elems[i].innerHTML = langData['pt'][group][i];
+    } else {
+      elems[i].innerHTML = langData[lang][group][i];
+    }
   }
 }
 
@@ -33,14 +35,22 @@ function updateLang() {
   updateGroupLang('headers', headers);
   updateGroupLang('toc', toc);
   updateGroupLang('content', content);
+
+  updateProjectsAlt();
 }
 
-function updateGroupLang(group, elems) {
-  for (let i = 0; i < elems.length; i += 1) {
-    if (langData[lang][group][i] === null) {
-      elems[i].innerHTML = langData['pt'][group][i];
-    } else {
-      elems[i].innerHTML = langData[lang][group][i];
-    }
+function changeLang(ev) {
+  let newLang = ev.target.innerText.toLowerCase();
+  if (newLang != lang) {
+    lang = newLang;
+    updateLang();
+  }
+}
+
+function updateProjectsAlt() {
+  let projects = [...document.querySelectorAll('.projectsgrid section')];
+  for (proj of projects) {
+    let name = proj.children[0].children[0].children[1].innerText;
+    proj.children[2].children[0].children[0].alt = name;
   }
 }
